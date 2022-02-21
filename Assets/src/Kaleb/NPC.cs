a4d28c18b22bf06e82a3c27072d7d7d6ad62ae0a
brow8820 <kalebbrowning14@gmail.com>
2022-02-21 12:02:48 -0800

    Added tracking to my files

---
+++ Assets/src/Kaleb/NPC.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class NPC : MonoBehaviour
+{
+    [SerializeField]
+    private int health;
+
+    [SerializeField]
+    int base_atk;
+    
+    Vector2 position; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public abstract void Move();
+}
