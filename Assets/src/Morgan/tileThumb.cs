ea5426c7b2b3c6bca6b373f621aa1f995c13ca45
SiriusCrack <MorganBrockman1998@Gmail.com>
2022-03-03 20:33:52 -0800

    created basic tile inventory with clack&drag duplication

--- Assets/src/Morgan/tileThumb.cs
+++ Assets/src/Morgan/tileThumb.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class tileThumb : MonoBehaviour
 {
-   void OnMouseDrag() {
-      transform.position = GetMousePos();
-   }
+    [SerializeField] private GameObject Tile;
+    [SerializeField] private Transform thumbPos;
 
-   Vector3 GetMousePos() {
-      var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-      mousePos.z = 0;
-      return mousePos;
-   }
+    private GameObject draggedTileThumb;
+
+    void OnMouseDown() {
+        draggedTileThumb = Instantiate(Tile, thumbPos.position, Quaternion.identity, thumbPos);
+    }
+
+    void OnMouseDrag() {
+        draggedTileThumb.transform.position = GetMousePos();
+    }
+
+    void OnMouseUp() {
+        Destroy(draggedTileThumb);
+    }
+    
+    Vector3 GetMousePos() {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        return mousePos;
+    }
 }
