38b8e89c87a89d2e3f8f3f8967ae65182cf7ff90
RRKirkpatrick <98623069+RRKirkpatrick@users.noreply.github.com>
2022-03-03 18:55:34 -0800

    Started equipment

---
+++ Assets/src/Ryan/Equipment.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equipment : MonoBehaviour
+{
+    /*
+    void Equiped()
+    {
+        Item HelmetSlot =   new Item { itemType = Item.ItemType.Helmet, amount = 1 };
+        Item ChestSlot =    new Item { itemType = Item.ItemType.Chest,  amount = 1 };
+        Item LegSlot =      new Item { itemType = Item.ItemType.Legs, amount = 1 };
+        Item MainHandSlot = new Item { itemType = Item.ItemType.Sword, amount = 1 };
+        Item OffHandSlot =  new Item { itemType = Item.ItemType.Shield, amount = 1 };
+        Item BootsSlot =    new Item { itemType = Item.ItemType.Boots, amount = 1 };
+    }
+
+
+    public void EquipItem(Item item, int slot)
+    {
+        if (item.itemType = Item.ItemType.Helmet)
+        {
+            if (HelmetSlot.ItemType = Item.ItemType.Empty)
+            {
+
+            }
+        }
+        //OnItemListChanged?.Invoke(this, EventArgs.Empty);
+        //Debug.Log(itemList.Count);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
--- Assets/src/Ryan/Item.cs
+++ Assets/src/Ryan/Item.cs
@@ -6,11 +6,13 @@ public class Item
 {
     public enum ItemType
     {
+        Empty,
         Sword,
         Shield,
         Boots,
         Helmet,
         Chest,
+        Legs,
     }
 
     public ItemType itemType;
@@ -21,11 +23,13 @@ public class Item
         switch (itemType)
         {
             default:
+            case ItemType.Empty:    return ItemAssets.Instance.swordSprite;
             case ItemType.Sword:    return ItemAssets.Instance.swordSprite;
             case ItemType.Shield:   return ItemAssets.Instance.shieldSprite;
             case ItemType.Boots:    return ItemAssets.Instance.bootsSprite;
             case ItemType.Helmet:   return ItemAssets.Instance.helmetSprite;
             case ItemType.Chest:    return ItemAssets.Instance.chestSprite;
+            case ItemType.Legs:     return ItemAssets.Instance.swordSprite;
         }
     }
 }
