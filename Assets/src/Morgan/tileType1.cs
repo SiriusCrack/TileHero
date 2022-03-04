8b07c7e4549a1da38948d2fe8d0672579df1489b
SiriusCrack <MorganBrockman1998@Gmail.com>
2022-03-03 19:38:05 -0800

    updated camelCase

--- Assets/src/Morgan/tileType1.cs
+++ Assets/src/Morgan/tileType1.cs
@@ -2,5 +2,5 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileType1 : levelTile {
+public class tileType1 : levelTile {
 }
--- Assets/src/Morgan/tileType2.cs
+++ Assets/src/Morgan/tileType2.cs
@@ -2,5 +2,5 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileType2 : levelTile {
+public class tileType2 : levelTile {
 }
