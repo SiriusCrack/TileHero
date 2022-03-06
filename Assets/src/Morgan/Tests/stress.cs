3899e282b4fb55d1d8210f482e3520606760b0ae
SiriusCrack <MorganBrockman1998@Gmail.com>
2022-03-05 20:47:04 -0800

    added Tests and EditMode folders

--- Assets/src/Morgan/Tests/stress.cs
+++ Assets/src/Morgan/Tests/stress.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class stress : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] tileFactory tileFactory;
+    private float x = -1.5f;
+    private float y = 4.7f;
+    private float width = 8f;
+    //private float height = -0.03f;
+    private int tile;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        var spawnedTile = tileFactory.GetNewTile(tile);
+        spawnedTile.name = $"{x},{y}";
+        spawnedTile.transform.position = new Vector2(x,y);
+        if(x > width) {
+            x = -1.5f;
+        }
+        tile++;
+        tile = tile%3;
+        x += 0.01f;
+        y -= 0.0001f;
     }
 }
