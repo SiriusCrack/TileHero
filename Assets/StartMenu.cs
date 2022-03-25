47be2ff83ce1b68be41845c456322ecabb500fe7
bpwalko <98635894+bpwalko@users.noreply.github.com>
2022-03-24 22:25:21 -0700

    Simple Start Menu (no music)
    I'm going to push this while it works then work on music

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitProgram()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
