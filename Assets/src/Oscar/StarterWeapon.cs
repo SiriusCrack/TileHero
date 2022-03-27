bec60e14e4ba6776544598c2c14f5df17b66f855
OscarTheGrouch2 <oscarmichua6@gmail.com>
2022-03-27 15:19:30 -0700

    Did some coding standards and updated weapon code

--- Assets/src/Oscar/StarterWeapon.cs
+++ Assets/src/Oscar/StarterWeapon.cs
@@ -1,20 +1,28 @@
+/*
+ * Filename: StarterWeapon.cs
+ * Developer: Oscar Michua-Zarate
+ * Purpose: This file standardizes how specidic weapons will work.
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static Weapon;
 
 
-
+/*
+ * Summary: Updates the stats for the StarterWeapon
+ * Member Variables:
+ * None
+ */
 public class StarterWeapon : Weapon
 {
 
     // Start is called before the first frame update
     void Start()
     {
-        atk_damage = 5;
-        atk_speed = 10;
-        range = 5;
-        effect = null;
+        updateSpeed(10);
+        updateDamage(5);
+        updateRange(5);
     }
 
     // Update is called once per frame
@@ -22,9 +30,5 @@ public class StarterWeapon : Weapon
     {
         
     }
-
-
-
-
 
 }
--- Assets/src/Oscar/Weapon.cs
+++ Assets/src/Oscar/Weapon.cs
@@ -1,18 +1,31 @@
+/*
+ * Filename: Weapon.cs
+ * Developer: Oscar Michua-Zarate
+ * Purpose: This file standardizes how weapons will work.
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Summary: Creates a weapon class with blank inputs 
+ *
+ * Member Variables:
+ * atkSpeed - Int value of attack speed
+ * atkDamage - Int value of attack damage
+ * atkRange - Int value of attack range
+ * effect - Place holder for a effect type script
+ */
 public class Weapon : MonoBehaviour
 {
+    [SerializeField]
+    public float atkSpeed;
 
     [SerializeField]
-    public int atk_speed;
+    public float atkDamage;
 
     [SerializeField]
-    public int atk_damage;
-
-    [SerializeField]
-    public int range;
+    public float atkRange;
 
     public Effect effect = null;
 
@@ -29,27 +42,70 @@ public class Weapon : MonoBehaviour
         
     }
 
-    //public void Useweapon(npc target)
-    //{
-    //    target.health -= damage;
-    //    if(effect != null)
-    //    {
-    //        applyEffect(target)
-    //    }
-    //}
-
     //public void applyEffect(Effect effectType, Enemy target)
     //{
         
     //}
 
-    public void weaponSpeed(int speed)
+
+/*
+ * Summary: Updates the weapon damage to match the given input
+ *
+ * Parameters:
+ * damage - inputs a float to give damage value
+ *
+ * Returns:
+ * None
+ */   
+    public void UpdateDamage(float damage)
+    {
+        atkDamage = damage;
+    }
+
+/*
+ * Summary: Updates the weapon speed to match the given input
+ *
+ * Parameters:
+ * speed - inputs a float to give speed value
+ *
+ * Returns:
+ * None
+ */   
+    public void UpdateSpeed(float speed)
+    {
+        atkSpeed = speed;
+    }
+
+/*
+ * Summary: Updates the weapon range to match the given input
+ *
+ * Parameters:
+ * range - inputs a float to give range value
+ *
+ * Returns:
+ * None
+ */   
+    public void UpdateRange(float range)
+    {
+        atkRange = range;
+    }
+
+/*
+ * Summary: Made for boundary test to check weapon speed
+ *
+ * Parameters:
+ * speed - inputs a int to check if atkSpeed is working properly
+ *
+ * Returns:
+ * 
+ */ 
+    public void WeaponSpeed(int speed)
     {
         if(speed <= 0){
             speed = 5;
             return;
         }
-        atk_speed = speed;
+        atkSpeed = speed;
     }
 
 }
