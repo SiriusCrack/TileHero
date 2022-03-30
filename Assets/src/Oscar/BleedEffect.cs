7265a33d2de98a9b5c29c8846c2778d4a349eb7f
OscarTheGrouch2 <oscarmichua6@gmail.com>
2022-03-29 19:27:20 -0700

    update v2

--- Assets/src/Oscar/BleedEffect.cs
+++ Assets/src/Oscar/BleedEffect.cs
@@ -15,30 +15,17 @@ public class BleedEffect : Effect
     void Update()
     {
         CountAttacks();
-        //BleedEffectFunction();//1.2f);
+        //BleedEffectFunction(1.2f);
     }
 
 //only 1.05 - 1.2 for strength (5% health to 20% health taken away every 5 seconds)
 //will change in the future, will be based on # of hits instead of time
-    public void BleedEffectFunction()//float strength)
+    public void BleedEffectFunction(float strength)
     {
-        //float health;
-        //health = npc.health;
-            Debug.Log("EffectTime 3: ");   
-            //EffectTime(1);
-            Debug.Log("EffectTime 2: ");     
-            //EffectTime(1);
-            Debug.Log("EffectTime 1: ");    
-            //EffectTime(1);
-            //Debug.Log("Enemy Health = " + npc.health);
-            //health = npc.health;
-            //npc.health = health / strength;
-            //Debug.Log("New Enemy Health = " + npc.health);
-            Debug.Log("Again: ");  
+        float health;
+        health = npc.health;
+        npc.health = health / strength;
     }
-
-
-
 
     public void CountAttacks()
     {
@@ -53,6 +40,4 @@ public class BleedEffect : Effect
             }
         }
     }
-
-
 }
