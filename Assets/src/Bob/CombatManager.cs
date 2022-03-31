166382605281509eb0d7d1de01f3cddd34a8dacb
bpwalko <98635894+bpwalko@users.noreply.github.com>
2022-03-31 01:05:28 -0700

    Update CombatManager.cs

--- Assets/src/Bob/CombatManager.cs
+++ Assets/src/Bob/CombatManager.cs
@@ -12,43 +12,31 @@ public class CombatManager : MonoBehaviour
     //enemies array stores all the enemies the hero encounters
     public GameObject[] enemies;
 
-    //the inactiveEnemy is a dummy enemy for when the enemies array is empty to avoid null reference errors
-    //public GameObject inactiveEnemy;
-
-    //currentEnemy moves whenever an enemy dies, and if there are no enemies in the
-    //array it will update when an enemy is encountered, newEnemy is used when adding
-    //new enemies encountered by the hero
+    //currentEnemy specifies which enemy in the array the hero
+    //will attack
     int currentEnemy = 0;
-    //int newEnemy = 1;
 
-    //used to judge when combat starts
-    //float heroPos; <-Prolly unneeded
-    //float enemyPos; <- Prolly unneeded
-
+    //Specifies how many enemies are in the room when combat
+    //starts, allowing for loops to check all enemies
     int listLength;
 
     //tells if the hero is currently in combat
     bool inCombat;
 
+    //Used to create other attack commands
     AttackCommand attack;
 
     // Start is called before the first frame update
     void Start()
     {
-        //inactiveEnemy.gameObject.SetActive(false);
-        //enemies[0] = inactiveEnemy;
-        //heroPos = hero.transform.localPosition.y;
         inCombat = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Gets hero's and current enemy's position every frame
-        //heroPos = hero.transform.localPosition.x;                         <-Prolly unneeded
-        //enemyPos = enemies[currentEnemy].transform.localPosition.x;
 
-        //This code will run when the hero encounters an enemy
+        //This code will run when the hero enters a new room to get the enemies in the room
         if (hero.collectEnemy == true)
         {
             collectEnemy();
@@ -71,7 +59,6 @@ public class CombatManager : MonoBehaviour
             //if the hero's health drops to zero, remove them from the game
             if (hero.health == 0)
             {
-                Debug.Log("The hero fucking died lol");
                 hero.gameObject.SetActive(false);
                 inCombat = false;
             }
@@ -94,33 +81,17 @@ public class CombatManager : MonoBehaviour
                 hero.attackTimer = 0;
             }
         }
-
-        //If combat has not been engaged and the hero encounters an enemy, start combat
-        //if ((Mathf.Abs(heroPos - enemyPos) < 0.5) && (inCombat == false))
-        //{
-        //    enterCombat();
-        //}
     }
 
     //Handles removing an enemy
     void enemyDeath(int enemyIndex)
     {
-        //Prints that an enemy has died for debugging purposes
-        //Debug.Log("death", enemies[currentEnemy]);
-        //Debug.Log(enemies);
 
         //moves the enemy out of the game so the hero doesn't get stuck on them and sets them inactive
         enemies[enemyIndex].transform.Translate(1000, 1000, 0);
         enemies[enemyIndex].gameObject.SetActive(false);
-        //enemies[enemyIndex] = null;
 
-        //if there is an enemy queued, make them the next enemy, otherwise, the current enemy
-        //is set to the inactiveEnemy to avoid null references and the hero is taken out of combat
-        //if (enemies[currentEnemy + 1])
-        //{
-        //    currentEnemy++;
-        //}
-        //else
+        //resets the enemies remaining count each time an enemy dies then check all enemies again
         enemiesRemaining = 0;
         for(i = 0; i < listLength; i++)
         {
@@ -130,9 +101,9 @@ public class CombatManager : MonoBehaviour
             }
         }
 
+        //if all enemies have died, exit combat. Else, move to the next target
         if (enemiesRemaining == 0)
         {
-            //enemies[currentEnemy] = inactiveEnemy;
             inCombat = false;
             hero.EndCombat();
         }
@@ -158,14 +129,10 @@ public class CombatManager : MonoBehaviour
     //sets necessary values for variables upon combat start. kinda like the unity start() for combat
     void enterCombat()
     {
-        //debugging message
-        //Debug.Log("Combat begins");
-
-        //set hero and combat manager variables to in combat
-        //hero.StartCombat();
+        //allows combat manager to process combat
         inCombat = true;
 
-        //reset hero an enemy attack timers
+        //reset hero an enemy attack timers since they run constantly out of combat
         hero.attackTimer = 0;
         for (i = 0; i < enemies.Length; i++)
         {
@@ -174,41 +141,27 @@ public class CombatManager : MonoBehaviour
                 enemies[i].GetComponent<Enemy>().attackTimer = 0;
             }
         }
-        //enemies[currentEnemy].attackTimer = 0;
     }
 
     //Used to add enemies to the enemies array when a hero encounters them
     void collectEnemy()
     {
-        Debug.Log("Entered Collection");
-        //fills the earliest empty slot (newEnemy) with the enemy the hero encountered
+        //Gets the length of the list of enemies in the room
         listLength = hero.enemies.Count;
+        //Adds the enemies into the enemies array on combat manager
         for (i = 0; i < listLength; i++)
         {
-            Debug.Log("Iterating through array:");
             if (hero.enemies[i])
             {
-                Debug.Log("Found valid enemy:");
                 enemies[i] = hero.enemies[i];
             }
         }
         
+        //sets the current enemy to the first one
         currentEnemy = 0;
-        //enemies[newEnemy] = hero.currentEnemy;
-        //update newEnemy to the next empty slot
-        //newEnemy++;
-        //reset the collection flag on the hero
-
+        //resets the collectionflag
         hero.collectEnemy = false;
+        //begins combat
         enterCombat();
-        //if there is no enemy, have the hero begin fighting this enemy
-        //if (enemies[currentEnemy] == inactiveEnemy)
-        //{
-        //    currentEnemy++;
-        //}
-
-        //debug printouts
-        //Debug.Log("collection");
-        //Debug.Log(enemies[currentEnemy]);
     }
 }
