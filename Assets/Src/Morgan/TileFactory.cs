157927f09c133ecf961a82cbcf29d24cde2efeef
SiriusCrack <MorganBrockman1998@Gmail.com>
2022-04-16 20:44:20 -0700

    update comments to match coding standards

--- Assets/Src/Morgan/TileFactory.cs
+++ Assets/Src/Morgan/TileFactory.cs
@@ -1,12 +1,29 @@
+/*
+ * Filename: TileFactory.cs
+ * Developer: Morgan Brockman
+ * Purpose: This file acts as the factory for LevelTile prefab objects.
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Summary: Takes a prefab and produces a new GameObject
+ * 
+ * Member Variables:
+ * none.
+ */
 public class TileFactory : MonoBehaviour
 {
-    [SerializeField] GameObject tileTypeA, tileTypeB;
-    
-    
+    /*
+     * Summary: Initializes variables, such as the connected boolean, its enemy list, and its GridManager.
+     * 
+     * Parameters:
+     * tile - The type of tile to be created.
+     * 
+     * Returns:
+     * GameObject - The created tile.
+     */
     public GameObject GetNewTile(GameObject tile)
     {
         return Instantiate(tile, new Vector3(0,0), Quaternion.identity);
