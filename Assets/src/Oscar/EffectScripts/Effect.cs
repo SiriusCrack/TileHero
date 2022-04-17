c2b8eda70b6e89f4373d248adb93a3589653bcdf
OscarTheGrouch2 <oscarmichua6@gmail.com>
2022-04-16 23:00:24 -0700

    Update effect comments

--- Assets/src/Oscar/EffectScripts/Effect.cs
+++ Assets/src/Oscar/EffectScripts/Effect.cs
@@ -1,18 +1,43 @@
+/*
+ * Filename: BuffEffect.cs
+ * Developer: Oscar Michua-Zarate
+ * Purpose: This file standardizes effects
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Threading;
 
+/*
+ * Summary: Creates effect 
+ *
+ * Member Variables:
+ * None
+ */
 public class Effect : MonoBehaviour
 {
-    float effectTime;
-    public float strength;
 
+/*
+ * Summary: Should overwrite a effect function
+ *
+ * Parameters:
+ * None
+ * Returns:
+ * None
+ */   
     public virtual void EffectFunction()
     {
 
     }
 
+/*
+ * Summary: Allow wait to occur in code
+ *
+ * Parameters:
+ * length - a variable to get length
+ * Returns:
+ * None 
+ */   
     //length ends up being in seconds
     public void EffectTime(int length)
     {
